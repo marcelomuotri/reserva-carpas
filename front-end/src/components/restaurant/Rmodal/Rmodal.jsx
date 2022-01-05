b5eb7f2e280300ec0848be5d7cd678df7e6b62b7
marcelomuotri <75761575+marcelomuotri@users.noreply.github.com>
2022-01-05 16:29:20 +0100

    agregando pizza

--- front-end/src/components/restaurant/Rmodal/Rmodal.jsx
+++ front-end/src/components/restaurant/Rmodal/Rmodal.jsx
@@ -1,24 +1,47 @@
-import React, { useContext, useState, useEffect } from 'react'
+import React, { useContext, useState, useRef } from 'react'
 import { Button, Modal, Form, Dropdown } from 'react-bootstrap';
 import { DataContext } from '../../context/DataProvider';
-import hamburguesa from '../../../assets/comida/hamburguesa.png'
+
 
 const Rmodal = () => {
 
     const value = useContext(DataContext);
 
     const [showRmodal, setShowRmodal] = value.showRmodal
-    const [base,setBase] = value.base
-    const [numero,setNumero] = value.numero
+    const [base, setBase] = value.base
+    const [numero, setNumero] = value.numero
     const [menu, setMenu] = value.menu
     const comprar = value.comprar
 
+    const [seleccion, setSeleccion] = useState(0)
+    const [nombre, setNombre] = useState('')
+    const [mostrarComplementos, setMostrarComplementos] = useState(false)
+    const [open, setOpen] = useState(false);
 
+    const animacion = useRef(null)
 
-    const handleClose = () => setShowRmodal(false);
+    const handleClose = () =>{
+     setShowRmodal(false);
+     setMostrarComplementos(false)
+     setNombre('')
+    }
     const cambiar = (numero) => {
         setNumero(numero)
     }
+
+    const seleccionar = (a, b) => {
+        setSeleccion(a)
+        setNombre(b)
+        setMostrarComplementos(true)
+        setOpen(true)
+        animacion.current.className = "animar"
+       
+    }
+
+    const borrar = () => {
+        animacion.current.className= "nada"
+    }
+
 
     return (
 
@@ -38,44 +61,58 @@ const Rmodal = () => {
                         </Dropdown.Toggle>
 
                         <Dropdown.Menu className="rmodal__dropdown">
-                        {base.map((item) => (
-                            <Dropdown.Item onClick={() => cambiar(item.numero)}>{item.numero} - {item.nombre}</Dropdown.Item>
-                        )
-                        )
-                        }
-                            
+                            {base.map((item) => (
+                                <Dropdown.Item onClick={() => cambiar(item.numero)}>{item.numero} - {item.nombre}</Dropdown.Item>
+                            )
+                            )
+                            }
+
                         </Dropdown.Menu>
                     </Dropdown>
 
                     <div className="flex">
                         {
                             menu.map((item) => (
-                                <div className="rmodal__contImagenes">
-                                    <img className="rmodal__imagenes" src={item.url} alt={item.nombre}></img>
-                                </div>
-                            ))
+                                <>
+                                    <div className={item.color + " rmodal__contImagen"} >
+                                        <img onMouseLeave={()=>borrar()} onClick={() => seleccionar(item.numero, item.nombre)} className="rmodal__imagenes" src={item.url} alt={item.nombre}></img>
+                                        <h3>${item.precio}</h3>
+                                    </div>
+                                </>
+                            )
+                            )
+                        }
+
+                    </div>
+                    <div>Seleccionaste {nombre}</div>
+                    <div ref={animacion}>
+                        {mostrarComplementos ?
+                            <div className="rmodal__complementos">
+                                {menu[seleccion].complementos.map((item) => (
+                                    <Button className={item.color}>
+                                        <h2>{item.complemento} - ${item.precio} </h2>
+                                    </Button>
+                                )
+                                )
+                                }
+                            </div>
+                            :
+                            <div></div>
                         }
                     </div>
-
-                    {/* <Form.Control
-
-                        className="agregar__input"
-                        size='lg'
-                        type="text"
-                        placeholder="Nombre"
-                    //onChange={e => setNombre(e.target.value)}
-
-                    /> */}
-                    
-
                 </Modal.Body>
+
+                <div>
+                    <p>Total</p>
+                </div>
 
                 <Modal.Footer className="tabla__contenedorBoton">
                     <Button onClick={() => comprar()} className="tabla__boton" >
-                        Alquilar
+                        Completar compra
                     </Button>
 
                 </Modal.Footer>
+
             </Modal>
         </div>
     )
